de58ba2718cc938cc14cea97fa75b217d84696b8
Subhajit Mitra <subhajitm61@gmail.com>
2020-04-21 17:28:26 +0530

    third commit - Class B modified and Class C added

--- src/com/company/ClassB.java
+++ src/com/company/ClassB.java
@@ -3,5 +3,6 @@ package com.company;
 public class ClassB {
     public static void main(String[] args) {
         System.out.println("Class B added");
+        System.out.println("Class B modified");
     }
 }
---
+++ src/com/company/ClassC.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class ClassC {
+    public static void main(String[] args) {
+        System.out.println("Class C added");
+    }
+}
